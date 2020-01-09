a480874be3d5e125dffac50d5986c77ea951c45d
Thomas Castleman <thomas7rex@gmail.com>
2020-01-09 15:08:21 -0500

    Scaling with drag and drop image upload

--- main.js
+++ main.js
@@ -10,83 +10,55 @@
  
 "use strict";
  
-let img, bg;
- 
+let src, bg, scaleFactor, c;
+
 function setup() {
-  createCanvas(800, 600).drop(gotFile);
-  //noStroke().fill(0xff).frameRate(10).noLoop();
+  resetCanvas(windowWidth, windowHeight)
+  c.drop(gotFile);
   textSize(24).textAlign(CENTER).blendMode(REPLACE);
-  bg = color(200);
 }
- 
+
 function draw() {
-  background(bg);
-  if (img) {
-    image(img, 0, 0, width, height);
+  if (src) {
+    // ensure image fits on screen
+    scale(scaleFactor);
+
+    // draw the uploaded image
+    image(src, 0, 0, src.width, src.height);
   } else {
     text('Drag an image file onto the canvas.', width >> 1, height >> 1);
   }
 }
- 
+
+// what to do when a file has been dropped on the canvas
 function gotFile(f) {
   if (f.type === 'image') {
-    img = loadImage(f.data, redraw);
+    src = loadImage(f.data, () => {
+      // what factor will we need to scale by to fit on screen
+      scaleFactor = windowHeight / src.height;
+
+      // set the canvas to be the same size as the scaled image
+      resetCanvas(src.width * scaleFactor, src.height * scaleFactor)
+      
+      // call draw again
+      redraw();
+    });
   } else {
     alert(`"${f.name}" isn't an image file!`);
   }
 }
 
-/* ---------------------------------------------------------------- */
+// reset the canvas to a given width and height
+function resetCanvas(w, h) {
+  if (c) c.remove();
+  c = createCanvas(w, h);
+	c.position(0, 50);
+  c.style('z-index', '1');
+}
 
-// let img;
-// let canvas;
-// let scaleFactor;
-
-// function setup() {
-//   resetCanvas(windowWidth, windowHeight)
-
-//   input = createFileInput(handleFile);
-//   input.position(0, 0);
-// }
-
-// function draw() {
-//   background(255);
-//   if (img) {
-//     // ensure image fits on screen
-//     scale(scaleFactor);
-
-//     // display image on the canvas
-//     image(img, 0, 0, img.width, img.height);
-//   }
-// }
-
-// // handle a file upload
-// function handleFile(file) {
-//   if (file.type === 'image') {
-//     img = createImg(file.data, '');
-//     img.hide(); // hide the HTML image element (we want to draw it on our canvas)
-
-//     // what factor will we need to scale by to fit on screen
-//     scaleFactor = windowHeight / img.height;
-
-//     // set the canvas to be the same size as the scaled image
-//     resetCanvas(img.width * scaleFactor, img.height * scaleFactor)
-//   } else {
-//     img = null;
-//   }
-// }
-
-// // reset the canvas to a given width and height
-// function resetCanvas(w, h) {
-//   if (canvas) canvas.remove();
-//   canvas = createCanvas(w, h);
-// 	canvas.position(0, 0);
-//   canvas.style('z-index', '-1');
-// }
-
-// // save the current canvas
-// function saveLowPoly() {
-//   let name = prompt('Please enter a file name', 'lowpoly')
-//   if (!name) name = 'lowpoly';
-//   save(name + '.png')
-// }
+// save the current canvas
+function saveLowPoly() {
+  let name = prompt('Please enter a file name', 'lowpoly')
+  if (!name) name = 'lowpoly';
+  save(name + '.png')
+}
